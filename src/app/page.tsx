993694f4ee3322751f4f56f535fa485b6d22564e
Yaremkevych <137343042+Yaremkevych@users.noreply.github.com>
2026-06-14 23:21:45 +0200

    add-mob-hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,8 @@ import styles from "./page.module.css";
 export default function Home() {
     return (
         <div className={styles.page}>
-            <main className={styles.main}>
-                <Hero />
-            </main>
+            <Hero />
+            <main className={styles.main}></main>
         </div>
     );
 }
--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,9 +1,17 @@
+import css from "./SearchBox.module.css";
+
 export default function SearchBox() {
     return (
-        <form>
-            <input type="text" placeholder="Search recipes" />
+        <form className={css.form}>
+            <input
+                className={css.input}
+                type="text"
+                placeholder="Search recipes"
+            />
 
-            <button type="submit">Search</button>
+            <button className={css.button} type="submit">
+                Search
+            </button>
         </form>
     );
 }
