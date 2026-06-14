d6fbfc8262aa9ec339a9638348bf254b669757b0
Yaremkevych <137343042+Yaremkevych@users.noreply.github.com>
2026-06-14 18:10:39 +0200

    start hero section

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,15 @@
+import SearchBox from "../SearchBox/SearchBox";
+
+export default function Hero() {
+    return (
+        <section>
+            <h1>
+                Plan, Cook, and
+                <br />
+                Share Your Flavors
+            </h1>
+
+            <SearchBox />
+        </section>
+    );
+}
---
+++ src/components/SearchBox/SearchBox.tsx
@@ -0,0 +1,9 @@
+export default function SearchBox() {
+    return (
+        <form>
+            <input type="text" placeholder="Search recipes" />
+
+            <button type="submit">Search</button>
+        </form>
+    );
+}
