99af4944187c66dabd75c2762b5e9bfb7956e77c
roman-yakubovskyi <yakubovskyi133@gmail.com>
2026-06-17 16:25:58 +0200

    fixed recipecard import

--- src/components/RecipeCard/RecipeCard.tsx
+++ src/components/RecipeCard/RecipeCard.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { useMemo } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import ClockIcon from "@/assets/icons/Time-clock.svg";
-import { Recipe } from "@/app/types/recipe";
+import { Recipe } from "@/lib/api/recipesApi";
 import { FavoriteRecipeButton } from "@/components/FavoriteRecipeButton/FavoriteRecipeButton";
 import styles from "./RecipeCard.module.css";
 
@@ -25,41 +24,39 @@ type Props = {
 };
 
 export const RecipeCard = ({ recipe, index = 0 }: Props) => {
-  const imageSrc = useMemo(
-    () => recipe.thumb || FALLBACK_IMAGES[index % FALLBACK_IMAGES.length],
-    [recipe.thumb, index],
-  );
+  const imageSrc =
+    recipe.image || FALLBACK_IMAGES[index % FALLBACK_IMAGES.length];
 
   return (
-    <li className={styles.recipeCard}>
+    <li className={styles.card}>
       <Image
         src={imageSrc}
         alt={recipe.title}
         width={337}
         height={230}
-        className={styles.recipeImage}
+        className={styles.img}
       />
 
-      <div className={styles.cardHeader}>
-        <h3 className={styles.recipeTitle}>{recipe.title}</h3>
+      <div className={styles.header}>
+        <h3 className={styles.title}>{recipe.title}</h3>
 
-        <div className={styles.timeBadge}>
-          <ClockIcon className={styles.timeIcon} />
+        <div className={styles.timeBox}>
+          <ClockIcon className={styles.clock} />
 
-          <span className={styles.timeText}>
-            {recipe.time ? `${recipe.time} min` : "—"}
+          <span className={styles.time}>
+            {recipe.cookingTime ? `${recipe.cookingTime} min` : "—"}
           </span>
         </div>
       </div>
 
-      <div className={styles.recipeInfo}>
+      <div className={styles.desBox}>
         <p>{recipe.description}</p>
 
         <p>Calories: {recipe.calories ? `~${recipe.calories} cals` : "—"}</p>
       </div>
 
-      <div className={styles.actions}>
-        <Link href={`/recipes/${recipe._id}`} className={styles.detailsButton}>
+      <div className={styles.btnBox}>
+        <Link href={`/recipes/${recipe._id}`} className={styles.moreBtn}>
           Learn more
         </Link>
 
