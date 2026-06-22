bb7a7ec0be2e8f08dd1bf1f72575e257a181dc9a
Ігор Яремкевич <jaremkiewicz1992@gmail.com>
2026-06-22 19:31:06 +0200

    fix_error_and_other_css

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,13 +1,29 @@
 "use client";
 
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, ErrorMessage, useField } from "formik";
 import * as Yup from "yup";
 import { useRecipesQueryParamsStore } from "@/lib/store/recipesQueryParamsStore";
 import css from "./SearchBox.module.css";
 
 const SearchSchema = Yup.object({
-    search: Yup.string().trim().max(16, "Search must be 16 characters or less"),
+    search: Yup.string()
+        .trim()
+        .max(16, "Search query must be 16 characters or fewer"),
 });
+
+const SearchInput = () => {
+    const [field, meta] = useField("search");
+    const hasError = Boolean(meta.touched && meta.error);
+
+    return (
+        <input
+            {...field}
+            className={`${css.input} ${hasError ? css.inputError : ""}`}
+            type="text"
+            placeholder="Search recipes"
+        />
+    );
+};
 
 export default function SearchBox() {
     const { search, setSearch } = useRecipesQueryParamsStore();
@@ -22,12 +38,7 @@ export default function SearchBox() {
             }}
         >
             <Form className={css.form}>
-                <Field
-                    className={css.input}
-                    type="text"
-                    name="search"
-                    placeholder="Search recipes"
-                />
+                <SearchInput />
 
                 <button className={css.button} type="submit">
                     Search
